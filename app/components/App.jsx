38ddc8beefff653c72219d5d95ca5cdd1dfb8ce3
Chris Laslett <chris@concreteplatform.com>
2016-07-27 12:07:50 +0100

    Simple note app part 1

---
+++ app/components/App.jsx
@@ -0,0 +1,84 @@
+import React from 'react';
+
+import Notes from './Notes';
+import uuid from 'uuid';
+
+export default class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			notes: [
+				{
+					id: uuid.v4(),
+					task: 'Learn React'
+				},
+				{
+					id: uuid.v4(),
+					task: 'Do laundry'
+				}
+			]
+		};
+	}
+
+	render() {
+		const {notes} = this.state;
+
+		return (
+			<div>
+				<button onClick={this.addNote}>+</button>
+				<Notes 
+					notes={notes}
+					onNoteClick={this.activateNoteEdit}
+					onEdit={this.editNote}
+					onDelete={this.deleteNote}
+					/>
+			</div>
+		)
+	}
+
+	addNote = () => {
+		this.setState({
+			notes: this.state.notes.concat([{
+				id: uuid.v4(),
+				task: 'New task'
+			}])
+		})
+	}
+
+	deleteNote = (id, e) => {
+		console.log(id);
+		e.stopPropagation();
+
+		this.setState({
+			notes: this.state.notes.filter(note => note.id !== id)
+		})
+	}
+
+	activateNoteEdit = (id) => {
+		console.log('activateNoteEdit', id);
+		this.setState({
+			notes: this.state.notes.map(note => {
+				if(note.id === id) {
+					note.editing = true;
+				}
+
+				return note;
+				})
+		});
+		console.log(this.state);
+	}
+
+	editNote = (id, task) => {
+		this.setState({
+			notes: this.state.notes.map(note => {
+				if(note.id === id) {
+					note.editing = false;
+					note.task = task;
+				}
+
+				return note;
+			})
+		});
+	}
+}
---
+++ app/components/Notes.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import Note from './Note';
+import Editable from './Editable';
+
+export default ({
+	notes, 
+	onNoteClick=() => {}, 
+	onEdit=() => {},
+	onDelete = () => {}
+}) => (
+	<ul>{notes.map(({id, task, editing}) =>
+		<li key={id}>
+			<Note onClick={onNoteClick.bind(null, id)}>
+				<Editable
+					editing={editing}
+					value={task}
+					onEdit={onEdit.bind(null, id)} />
+				<button onClick={onDelete.bind(null, id)}>x</button>
+			</Note>
+		</li>
+	)}</ul>
+)
