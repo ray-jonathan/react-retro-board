1681786fdca23cc3896f6c8a150053865ef8db75
Chris Laslett <chris@concreteplatform.com>
2016-07-27 13:51:17 +0100

    Styling

--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -4,19 +4,22 @@ import Note from './Note';
 import Editable from './Editable';
 
 export default ({
-	notes, 
-	onNoteClick=() => {}, 
+	notes,
+	onNoteClick=() => {},
 	onEdit=() => {},
 	onDelete = () => {}
 }) => (
-	<ul>{notes.map(({id, task, editing}) =>
+	<ul className="notes">{notes.map(({id, task, editing}) =>
 		<li key={id}>
-			<Note onClick={onNoteClick.bind(null, id)}>
+			<Note className="note" onClick={onNoteClick.bind(null, id)}>
 				<Editable
+					className="editable"
 					editing={editing}
 					value={task}
 					onEdit={onEdit.bind(null, id)} />
-				<button onClick={onDelete.bind(null, id)}>x</button>
+				<button
+					className="delete"
+					onClick={onDelete.bind(null, id)}>x</button>
 			</Note>
 		</li>
 	)}</ul>
