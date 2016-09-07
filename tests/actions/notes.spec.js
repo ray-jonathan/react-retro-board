0c9a8bdb4a54def67bfa484b0747af6afa5ca378
Chris Laslett <chris@concreteplatform.com>
2016-09-07 14:37:20 +0100

    Refactor tests to read better

--- tests/actions/notes.spec.js
+++ tests/actions/notes.spec.js
@@ -3,48 +3,64 @@ import uuid from 'uuid';
 import { addNote, activateNoteEditing, deleteNote, editNote } from '../../app/actions/notes';
 import * as types from '../../app/constants/actionTypes';
 
-describe('Note actions', () => {
-	
-	it('creates an action to add a note', ()=> {
-		const id = uuid.v4();
-		const expected = {
-			type: types.ADD_NOTE,
-			id
-		};
+describe('Notes actions', () => {
 
-		expect(addNote(id)).toEqual(expected);
+	describe(`${types.ADD_NOTE}`, () => {
+
+		it('creates an action to add a note', ()=> {
+			const id = uuid.v4();
+			const expected = {
+				type: types.ADD_NOTE,
+				id
+			};
+
+			expect(addNote(id)).toEqual(expected);
+		});
+
 	});
 
-	it('creates an action to delete a note', ()=> {
-		const id = uuid.v4();
-		const expected = {
-			type: types.DELETE_NOTE,
-			id
-		};
+	describe(`${types.DELETE_NOTE}`, () => {
 
-		expect(deleteNote(id)).toEqual(expected);
+		it('creates an action to delete a note', ()=> {
+			const id = uuid.v4();
+			const expected = {
+				type: types.DELETE_NOTE,
+				id
+			};
+
+			expect(deleteNote(id)).toEqual(expected);
+		});
+
 	});
 
-	it('creates an action to activiate note editing', ()=> {
-		const id = uuid.v4();
-		const expected = {
-			type: types.ACTIVATE_NOTE_EDITING,
-			id
-		};
+	describe(`${types.ACTIVATE_NOTE_EDITING}`, () => {
 
-		expect(activateNoteEditing(id)).toEqual(expected);
+		it('creates an action to activiate note editing', ()=> {
+			const id = uuid.v4();
+			const expected = {
+				type: types.ACTIVATE_NOTE_EDITING,
+				id
+			};
+
+			expect(activateNoteEditing(id)).toEqual(expected);
+		});
+
 	});
 
-	it('creates an action to edit note', ()=> {
-		const id = uuid.v4();
-		const task = 'Test ' + uuid.v4();
-		const expected = {
-			type: types.EDIT_NOTE,
-			id,
-			task
-		};
+	describe(`${types.EDIT_NOTE}`, () => {
 
-		expect(editNote(id, task)).toEqual(expected);
+		it('creates an action to edit a note', ()=> {
+			const id = uuid.v4();
+			const task = 'Test ' + uuid.v4();
+			const expected = {
+				type: types.EDIT_NOTE,
+				id,
+				task
+			};
+
+			expect(editNote(id, task)).toEqual(expected);
+		});
+
 	});
 
 });
