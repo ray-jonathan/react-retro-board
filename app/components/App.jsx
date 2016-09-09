ef428f2f2ebca6faa45ae6da24f127723bf3eda8
Chris Laslett <chris@concreteplatform.com>
2016-09-09 09:28:52 +0100

    Use react-bootstrap Navbar

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -3,15 +3,41 @@ import BoardContainer from './BoardContainer';
 import AddBoardButtonContainer from './AddBoardButtonContainer';
 import BoardSelectorContainer from './BoardSelectorContainer';
 
+import Navbar from 'react-bootstrap/lib/Navbar';
+import Nav from 'react-bootstrap/lib/Nav';
+import NavItem from 'react-bootstrap/lib/NavItem';
+
 export default () => (
 	<div className="app-wrapper">
 		<div className="app-header">
-			<h1>Retro-o-o-o-board</h1>
-			<BoardSelectorContainer />
-			<AddBoardButtonContainer label="Start a Retro..." />
+			<Navbar>
+				<Navbar.Header>
+					<Navbar.Brand>
+						<a href="#">Retro Board</a>
+					</Navbar.Brand>
+					<Navbar.Toggle />
+				</Navbar.Header>
+				<Navbar.Collapse>
+					<Nav>
+						<BoardSelectorContainer />
+						<AddBoardButtonContainer label={<span><i className="glyphicon glyphicon-plus"></i> Start Retro</span>} className="btn btn-primary navbar-btn"  />
+					</Nav>
+				</Navbar.Collapse>
+			</Navbar>
 		</div>
 		<div className="app-body">
 			<BoardContainer />
 		</div>
 	</div>
+
+	// <div className="app-wrapper">
+	// 	<div className="app-header">
+	// 		<h1>Retro</h1>
+	// 		<BoardSelectorContainer />
+	// 		<AddBoardButtonContainer label="Start new Retro..." />
+	// 	</div>
+	// 	<div className="app-body">
+	// 		<BoardContainer />
+	// 	</div>
+	// </div>
 )
