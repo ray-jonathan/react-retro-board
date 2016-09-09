53ca46373a44e9e345064d2ea9f637bb51c977fa
Chris Laslett <chris@concreteplatform.com>
2016-09-09 09:26:42 +0100

    Add loaders for images & fonts

--- libs/parts.js
+++ libs/parts.js
@@ -126,8 +126,51 @@ exports.setupCSS = function(paths) {
       loaders: [
         {
           test: /\.css$/,
-          loaders: ['style', 'css'],
-          include: paths
+          loaders: ['style', 'css']
+          //,
+          //include: paths
+        }
+      ]
+    }
+  };
+}
+
+exports.setupImages = function(paths) {
+  return {
+    module: {
+      loaders: [
+        { 
+          test: /\.png$/,
+          loader: "url-loader?limit=100000"
+        },
+        { 
+          test: /\.jpg$/,
+          loader: "file-loader"
+        }
+      ]
+    }
+  };
+}
+
+exports.setupFonts = function(paths) {
+  return {
+    module: {
+      loaders: [
+        {
+          test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, 
+          loader: 'url?limit=10000&mimetype=application/font-woff'
+        },
+        {
+          test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, 
+          loader: 'url?limit=10000&mimetype=application/octet-stream'
+        },
+        {
+          test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, 
+          loader: 'file'
+        },
+        {
+          test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, 
+          loader: 'url?limit=10000&mimetype=image/svg+xml'
         }
       ]
     }
