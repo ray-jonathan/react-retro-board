0a74ca298d160acf93bd2cae1ec8c2013c63f176
Chris Laslett <chris@concreteplatform.com>
2016-09-16 18:17:02 +0100

    Add unit test

---
+++ tests/actions/boards.spec.js
@@ -0,0 +1,53 @@
+import expect from 'expect';
+import uuid from 'uuid';
+import { addBoard, addLane, addLaneToBoard, setActiveBoard } from '../../app/actions/boards';
+import * as types from '../../app/constants/actionTypes';
+
+describe('Lanes actions', () => {
+
+	describe(`${types.ADD_BOARD}`, () => {
+
+		it('creates an action to add a board', ()=> {
+			const id = uuid.v4();
+			const expected = {
+				type: types.ADD_BOARD,
+				id,
+				name: id
+			};
+
+			expect(addBoard(id)).toEqual(expected);
+		});
+
+	});
+
+	describe(`${types.ASSIGN_LANE_TO_BOARD}`, () => {
+
+		it('creates an action to assign a lane to a board', ()=> {
+			const laneId = uuid.v4();
+			const boardId = uuid.v4();
+			const expected = {
+				type: types.ASSIGN_LANE_TO_BOARD,
+				laneId,
+				boardId
+			};
+
+			expect(addLaneToBoard(laneId, boardId)).toEqual(expected);
+		});
+
+	});
+
+	describe(`${types.SET_ACTIVE_BOARD}`, () => {
+
+		it('creates an action to set a board as active', ()=> {
+			const id = uuid.v4();
+			const expected = {
+				type: types.SET_ACTIVE_BOARD,
+				id
+			};
+
+			expect(setActiveBoard(id)).toEqual(expected);
+		});
+
+	});
+
+});
