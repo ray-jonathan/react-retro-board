49f9b642420f13ea1bec3d48b261bc3e57ec0856
Chris Laslett <chris@concreteplatform.com>
2016-09-20 15:09:06 +0100

    Add router

--- app/components/Root.jsx
+++ app/components/Root.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { Router, Route, browserHistory } from 'react-router';
 import App from './App';
 
 export default ({store}) => (
 	// react-redux Provider...
 	//makes the Redux store available to connect() calls in the entire component hierarchy
-	<Provider store={store}> 
-		<App />
+	<Provider store={store}>
+		<Router history={browserHistory}>
+			<Route path="/(board/:boardId)" component={App} />
+		</Router>
 	</Provider>
 )
