b42a897ec8d6c88eec0f7bf67ff5d47d6ec99b95
Kristen Marsh <kmarsh2@digital.ai>
2021-11-17 15:11:16 -0500

    made card layout marginally better ;)

--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -4,37 +4,58 @@ import Button from './Button';
 import EditableInline from './EditableInline';
 
 export default ({ notes, onDelete, onNoteClick, onFinishEdit, onNoteMove }) => {
-	return(
+	return (
 	<ul className="notes list-unstyled">{notes.map( ({ id, task, editing }, i) => {
+		if(editing) {
+			return (
+				<li key={id} className="note">
+					<Note
+						id={id}
+						index={i}
+						className="note"
+						name="task"
+						task={task}
+						onClick={ () => onNoteClick(id) }
+						onMove={ (sourceIndex, targetIndex) => onNoteMove(sourceIndex, targetIndex) }>
 		
-		console.log('note:', {id, task, editing})
-		return (<li key={id} className="note">
-
-			<Button 
-				onClick={ () => onDelete(id) }
-				label="x" 
-				className="delete btn btn-default pull-right" />
-
-			<Note
-				id={id}
-				index={i}
-				className="note"
-				name="task"
-				task={task}
-				onClick={ (e) => {
-					console.log('target: ',e.target)
-					onNoteClick(id)
-				} }
-				onMove={ (sourceIndex, targetIndex) => onNoteMove(sourceIndex, targetIndex) }>
-
-				<EditableInline 
-					editing={editing} 
-					value={task} 
-					onEdit={ (task) => onFinishEdit(id, task) } />
-
-			</Note>
-
-		</li>)}
-	)}</ul>
+						<span 
+							className="card-input" 
+							role="textbox" 
+							id={id}
+							contentEditable>
+							{task}
+						</span>
+					</Note>
+					<button value={id} id={id} className='btn-primary' onClick={() => onFinishEdit(id, document.getElementById(id).innerText)}>Save</button>
+					<Button 
+						onClick={ () => onDelete(id) }
+						label="x" 
+						className="delete btn btn-default pull-right" />
+				</li>)
+		} else {
+			return (
+				<li key={id} className="note">
+					<Note
+						id={id}
+						index={i}
+						className="note"
+						name="task"
+						task={task}
+						onClick={ () => onNoteClick(id) }
+						onMove={ (sourceIndex, targetIndex) => onNoteMove(sourceIndex, targetIndex) }>
+		
+						<span 
+							className="card-display" 
+							role="textbox" 
+							id="note-content">
+							{task}
+						</span>
+					</Note>
+				</li>
+			)
+		}
+		
+	}
+	)}</ul> 
 )}
 
